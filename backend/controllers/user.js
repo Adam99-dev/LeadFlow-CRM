1023b605b392bdb7932489ff196891dd3f813114
Syed Adam Ali <kingsilverali@gmail.com>
2026-05-30 15:03:06 +0530

    cross origin issue resolved

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -56,7 +56,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -131,7 +131,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
