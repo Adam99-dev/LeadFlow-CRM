70c360b81f17ce1a5bd43c0e7b4d9f32e00481ac
Syed Adam Ali <kingsilverali@gmail.com>
2026-05-30 15:12:07 +0530

    some final changes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -55,7 +55,7 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
@@ -130,7 +130,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
